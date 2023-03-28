3c8d02ee27f86ac95a8ae8b8a2e358fcfc73bf8a
Viccart <hannahcarter89@hotmail.com>
2023-03-28 15:32:31 +0100

    removing comments from app.js file

--- app.js
+++ app.js
@@ -22,18 +22,3 @@ app.use(handlePSQLErrors);
 app.use(handleCustomErrors);
 app.use(handle500Errors);
 module.exports = app;
-
-// Responds with:
-
-// an articles array of article objects, each of which should have the following properties:
-// author --
-// title --
-// article_id --
-// topic --
-// created_at --
-// votes --
-// article_img_url --
-// comment_count which is the total count of all the comments with this article_id - you should make use of queries to the database in order to achieve this.
-// the articles should be sorted by date in descending order.
-
-//NOT BODY
