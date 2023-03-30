90035fa79e484fcc350e098cb372d8797f3206d9
Viccart <hannahcarter89@hotmail.com>
2023-03-30 14:35:05 +0100

    task 7

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const {
   getArticleById,
   getArticles,
   getArticleCommentsById,
+  postNewCommentToArticleID,
 } = require("./controllers/articles.controllers");
 const {
   handleInvalidPath,
@@ -12,11 +13,13 @@ const {
   handle500Errors,
 } = require("./error");
 const app = express();
+app.use(express.json());
 
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id/comments", getArticleCommentsById);
+app.post("/api/articles/:article_id/comments", postNewCommentToArticleID);
 
 app.all("/*", handleInvalidPath);
 
--- error.js
+++ error.js
@@ -5,6 +5,8 @@ exports.handleInvalidPath = (req, res) => {
 exports.handlePSQLErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid article id" });
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "Username or article id does not exist" });
   } else {
     next(err);
   }
@@ -20,5 +22,5 @@ exports.handleCustomErrors = (err, req, res, next) => {
 };
 
 exports.handle500Errors = (err, req, res, next) => {
-  if (err) res.status(500).send({ err });
+  if (err) console.log(err), res.status(500).send({ err }); // debugging 500s
 };
