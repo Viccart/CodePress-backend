5d72482db9ff1d543eec7c8903510d69c410849c
Viccart <hannahcarter89@hotmail.com>
2023-04-17 10:49:29 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
 const {
@@ -16,6 +17,8 @@ const {
 const { deleteCommentById } = require("./controllers/comments.controllers");
 const { getUsers } = require("./controllers/users.controllers");
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
